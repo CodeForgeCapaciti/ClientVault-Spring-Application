3bb75fcbc01d473e90387c24ab55e1c577e9d01b
makhotso <thato.makhotso@gmail.com>
2026-02-02 13:52:05 +0200

    This is the first phase of the application

---
+++ src/main/java/com/codeForge/clientVault/ClientVaultApplication.java
@@ -0,0 +1,13 @@
+package com.codeForge.clientVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeForge/clientVault/ClientVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeForge.clientVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
