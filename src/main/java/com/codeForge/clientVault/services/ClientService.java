d5d0cfd6af440028cb660a4755a6d1944d9aac13
Susan04616 <susansusie04@gmail.com>
2026-02-03 13:07:07 +0200

    Updated the Service Class(Delete and Get Methods)

--- src/main/java/com/codeForge/clientVault/services/ClientService.java
+++ src/main/java/com/codeForge/clientVault/services/ClientService.java
@@ -6,17 +6,37 @@ import com.codeForge.clientVault.repo.ClientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClientService {
 
-public final ClientRepo clientRepo;
+    public final ClientRepo clientRepo;
 
     public ClientService(ClientRepo clientRepo) {
         this.clientRepo = clientRepo;
     }
 
+    //Returns a list of all clients
+    public List<Client> getAllClients() {
+        return clientRepo.findAll();
+    }
+
+    //Get a client by ID
+    public Client getClientById(Long clientId) {
+        return clientRepo.findById(clientId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "The client with id " + clientId + " does not exist"
+                ));
+    }
+
+    //To delete a client
+    public void deleteClient(Client client) {
+        clientRepo.delete(client);
+    }
+
     //To create a new client
-    public Client createClient(Client client){
+    public Client createClient(Client client) {
         return clientRepo.save(client);
     }
 
