23e5fac3842780f8ff23d00b27bb568d54a72c49
Susan04616 <susansusie04@gmail.com>
2026-02-03 11:12:47 +0200

    added a constructor for the repo class inside service

--- src/main/java/com/codeForge/clientVault/services/ClientService.java
+++ src/main/java/com/codeForge/clientVault/services/ClientService.java
@@ -1,4 +1,17 @@
 package com.codeForge.clientVault.services;
 
+
+import com.codeForge.clientVault.repo.ClientRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClientService {
+
+public final ClientRepo clientRepo;
+
+    public ClientService(ClientRepo clientRepo) {
+        this.clientRepo = clientRepo;
+    }
+
 }
